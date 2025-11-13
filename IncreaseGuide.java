3e6d112f0c80cf2da08499fcf0e163e1d695ac04
Samuel P <robbsnark@gmail.com>
2025-11-13 22:29:21 +0100

    Changed printout to skip increaseCount on row 1 (instead of printing "dc 0")

--- IncreaseGuide.java
+++ IncreaseGuide.java
@@ -1,9 +1,21 @@
 public class IncreaseGuide {
     public static int increaseGuide(int circleSize) {
-        for (int i = 1; i < 10; i++) {
+
+        // BREAKDOWN OF VARIABLES:
+        // currentRow - Specifies rows of crochet in ascending order
+        // circleSize - The number the user put in the terminal
+        // increaseCount - Determines how many dc stitches needed each row
+
+
+        for (int i = 1; i < 11; i++) {
             int currentRow = circleSize * i;
             int increaseCount = i - 1;
-            System.out.println("Row " + i + ": " + "(2dc, dc " + increaseCount + ") x " + circleSize + " (" + currentRow + ")");
+            String printResult = "Row " + i + ": " + "(2dc, dc " + increaseCount + ") x " + circleSize + " (" + currentRow + ")";
+            if (increaseCount > 0) {
+               System.out.println(printResult);
+            } else {
+                System.out.println("Row " + i + ": " + "(2dc) x " + circleSize + " (" + currentRow + ")");
+            }
 
             // Below is the original counter's printout
             // System.out.println("Row " + i + ": " + currentRow + " stitches");
