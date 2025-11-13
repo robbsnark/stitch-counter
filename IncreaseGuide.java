4b4f111a5604a2d1f446d926ed652d1f9102ad9a
Samuel P <robbsnark@gmail.com>
2025-11-14 00:32:15 +0100

    Added doubleCrochet and halfDoubleCrochet methods

--- IncreaseGuide.java
+++ IncreaseGuide.java
@@ -27,43 +27,73 @@ public class IncreaseGuide {
         // Suggested starting stitches for sc: 6-8
         // Maybe an idea to have the user pick 6 or 8? 
         // But honestly the difference is so small I might skip it
-
         // This way of printing the first row is pretty manual/hardcodey:
 
         int scSmall = 6;
         int scLarge = 8;
-
         String startingRow = "Row 1: " + scSmall + "sc in MR (" + scSmall + ")";
         System.out.println(startingRow);
 
         // I could've done this with Row 2 as well, writing something like this right here:
         // String secondRow = "Row 2: (2dc) x " + scSmall + "(" + scSmall * 2 + ")";
         // System.out.println(secondRow);
-
         // But I chose to bake that in with the rest of the pattern,
         // using an if statement to ultimately control only that row
         
         for (int i = 2; i < 11; i++) {
-            int startingStitch = i * 6;
+            int startingStitch = i * scSmall;
             int increaseCount = i - 1;
-            String printResult = "Row " + i + ": " + "(2dc, dc " + increaseCount + ") x " + 6 + " (" + startingStitch + ")";
+            String printResult = "Row " + i + ": " + "(2sc, sc " + increaseCount + ") x " + scSmall + " (" + startingStitch + ")";
 
-            if (increaseCount > 1) {
+            if (increaseCount >= 1) {
                System.out.println(printResult);
             } else {
-                System.out.println("Row " + i + ": " + "(2dc) x " + 6 + " (" + startingStitch + ")");
+                System.out.println("Row " + i + ": " + "(2sc) x " + scSmall + " (" + startingStitch + ")");
             }
         }
     }
 
     public static void halfDoubleCrochet(String stitchPick) {
         // Suggested starting stitches for hdc: 8-10
-        System.out.println("hdc");
+
+        int hdcSmall = 8;
+        int hdcLarge = 10;
+        String startingRow = "Row 1: " + hdcSmall + "hdc in MR (" + hdcSmall + ")";
+        System.out.println(startingRow);
+        
+        for (int i = 2; i < 11; i++) {
+            int startingStitch = i * hdcSmall;
+            int increaseCount = i - 1;
+            String printResult = "Row " + i + ": " + "(2hdc, hdc " + increaseCount + ") x " + hdcSmall + " (" + startingStitch + ")";
+
+            if (increaseCount >= 1) {
+               System.out.println(printResult);
+            } else {
+                System.out.println("Row " + i + ": " + "(2hdc) x " + hdcSmall + " (" + startingStitch + ")");
+            }
+        }
     }
 
     public static void doubleCrochet(String stitchPick) {
         // Suggested starting stitches for dc: 10-12
-        System.out.println("dc");
+
+        int dcSmall = 10;
+        int dcLarge = 12;
+        String startingRow = "Row 1: " + dcSmall + "dc in MR (" + dcSmall + ")";
+        System.out.println(startingRow);
+        
+        for (int i = 2; i < 11; i++) {
+            int startingStitch = i * dcSmall;
+            int increaseCount = i - 1;
+            String printResult = "Row " + i + ": " + "(2dc, dc " + increaseCount + ") x " + dcSmall + " (" + startingStitch + ")";
+
+            if (increaseCount >= 1) {
+               System.out.println(printResult);
+            } else {
+                System.out.println("Row " + i + ": " + "(2dc) x " + dcSmall + " (" + startingStitch + ")");
+            }
+        }
+
     }
 
 
