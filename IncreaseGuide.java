d52f43f2527fced1dc6bc1d6c698949499ae142b
Samuel P <robbsnark@gmail.com>
2025-11-14 00:05:14 +0100

    Rewrote most of everything; mainly changing the user's input to be stitch type instead of size

--- IncreaseGuide.java
+++ IncreaseGuide.java
@@ -1,12 +1,12 @@
 public class IncreaseGuide {
-    public static int increaseGuide(int circleSize) {
+    public static void increaseGuide() {
 
         // BREAKDOWN OF VARIABLES:
+        // stitchPick - The type of preferred stitch the user put in the terminal
         // currentRow - Specifies rows of crochet in ascending order
-        // circleSize - The number the user put in the terminal
-        // increaseCount - Determines how many dc stitches needed each row
+        // increaseCount - Determines how many dc stitches are needed for each row
 
-
+        /*
         for (int i = 1; i < 11; i++) {
             int currentRow = circleSize * i;
             int increaseCount = i - 1;
@@ -20,6 +20,51 @@ public class IncreaseGuide {
             // Below is the original counter's printout
             // System.out.println("Row " + i + ": " + currentRow + " stitches");
 
-        } return circleSize;
+        } return circleSize; */
     }
+
+    public static void singleCrochet(String stitchPick) {
+        // Suggested starting stitches for sc: 6-8
+        // Maybe an idea to have the user pick 6 or 8? 
+        // But honestly the difference is so small I might skip it
+
+        // This way of printing the first row is pretty manual/hardcodey:
+
+        int scSmall = 6;
+        int scLarge = 8;
+
+        String startingRow = "Row 1: " + scSmall + "sc in MR (" + scSmall + ")";
+        System.out.println(startingRow);
+
+        // I could've done this with Row 2 as well, writing something like this right here:
+        // String secondRow = "Row 2: (2dc) x " + scSmall + "(" + scSmall * 2 + ")";
+        // System.out.println(secondRow);
+
+        // But I chose to bake that in with the rest of the pattern,
+        // using an if statement to ultimately control only that row
+        
+        for (int i = 2; i < 11; i++) {
+            int startingStitch = i * 6;
+            int increaseCount = i - 1;
+            String printResult = "Row " + i + ": " + "(2dc, dc " + increaseCount + ") x " + 6 + " (" + startingStitch + ")";
+
+            if (increaseCount > 1) {
+               System.out.println(printResult);
+            } else {
+                System.out.println("Row " + i + ": " + "(2dc) x " + 6 + " (" + startingStitch + ")");
+            }
+        }
+    }
+
+    public static void halfDoubleCrochet(String stitchPick) {
+        // Suggested starting stitches for hdc: 8-10
+        System.out.println("hdc");
+    }
+
+    public static void doubleCrochet(String stitchPick) {
+        // Suggested starting stitches for dc: 10-12
+        System.out.println("dc");
+    }
+
+
 }
--- Program.java
+++ Program.java
@@ -5,21 +5,61 @@ public class Program {
     public void run() {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("How wide would you like your circle to be?");
-        int circleSize = scan.nextInt();
-
-        while (circleSize < 6) {
-            System.out.println("That's too small for this size yarn.");
-            System.out.println("How wide would you like your circle to be?");
-            circleSize = scan.nextInt();
-        } 
-        while (circleSize % 2 != 0) {
-            System.out.println("Please choose an even number.");
-            System.out.println("How wide would you like your circle to be?");
-            circleSize = scan.nextInt();   
+        while (true) {
+            System.out.println("What's your preferred stitch to use? \n" +
+            "SC - Single crochet \n" + 
+            "HDC - Half double crochet \n" +
+            "DC - Double crochet");
+            String stitchPick = scan.nextLine();
+            
+            switch (stitchPick.toUpperCase()) {
+                case "SC":
+                    IncreaseGuide.singleCrochet(stitchPick);
+                    break;
+                case "HDC":
+                    IncreaseGuide.halfDoubleCrochet(stitchPick);
+                    break;
+                case "DC":
+                    IncreaseGuide.doubleCrochet(stitchPick);
+                    break;
+                default:
+                    System.out.println("Sorry, I didn't catch that.");
+                    continue;
+            } 
+            break;
         }
 
-        int increaseCounter = IncreaseGuide.increaseGuide(circleSize);
+
+        // OLD CODE
+
+        // If-else version of stitchPick switchcase:
+
+        // if (stitchPick.equals("SC")){
+        //     IncreaseGuide.singleCrochet(stitchPick);
+        // } else if (stitchPick.equals("HDC")) {
+        //     IncreaseGuide.halfDoubleCrochet(stitchPick);
+        // } else if (stitchPick.equals("DC")) {
+        //     IncreaseGuide.doubleCrochet(stitchPick);
+        // } else {
+        //     System.out.println("Sorry I didn't catch that.");
+        //     continue;
+        // } break;
+
+
+        // int circleSize = scan.nextInt();
+
+        // while (circleSize < 6) {
+        //     System.out.println("That's too small for this size yarn.");
+        //     System.out.println("How wide would you like your circle to be?");
+        //     circleSize = scan.nextInt();
+        // } 
+        // while (circleSize % 2 != 0) {
+        //     System.out.println("Please choose an even number.");
+        //     System.out.println("How wide would you like your circle to be?");
+        //     circleSize = scan.nextInt();   
+        // }
+
+        // int increaseCounter = IncreaseGuide.increaseGuide(circleSize);
 
         // Below is the original counter that only showed how many stitches needed per row
         // (and not which specific stitches to increase)
@@ -31,7 +71,6 @@ public class Program {
         } 
         */
 
-
-
     }
 }
+
